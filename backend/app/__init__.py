d7acd8d7e8cda53322232e3a55b923114eb9f6b0
CesarAP24 <cesar.perales@utec.edu.pe>
2023-11-26 14:47:50 -0500

    mis wbs contigo verilog digo AAAAAAAAAAAAAAAAAAAAA

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -545,6 +545,43 @@ def create_app(test_config=None):
         else:
             return jsonify({"success": True, "data": data, "token": sede['sede_id']}), code
 
+    #ruta para suscribir un correo
+    @app.route('/empresa/<tenant_id>/sedes/<sede_id>/suscribir/<email>', methods=['POST'])
+    def suscribir(tenant_id, sede_id, email):
+        code = 200
+        aws_access_key_id = config['aws_access_key_id']
+        aws_secret_access_key = config['aws_secret_access_key']
+        aws_session_token = config['aws_session_token']
+        region = 'us-east-1'
+        
+        try:
+            # Crear un cliente SNS utilizando boto3 y las credenciales proporcionadas
+            sns_client = boto3.client('sns', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, aws_session_token=aws_session_token, region_name=region)
+
+            # ARN del tema al que deseas suscribirte
+            topic_arn = "arn:aws:sns:us-east-1:859485701311:RegisterSender"
+
+            # Dirección del endpoint al que se enviarán las notificaciones
+            correo = email
+
+            # Configurar la suscripción utilizando el cliente SNS
+            response = sns_client.subscribe(
+                TopicArn=topic_arn,
+                Protocol="email",
+                Endpoint=correo
+            )
+
+            # Imprimir la respuesta del servidor
+            print(response)
+            data = response
+        except Exception as e:
+            print(sys.exc_info())
+            code = 500
+            return jsonify({"success": False, "message": "No se pudo suscribir el correo"}), code
+
+        return jsonify({"success": True, "data": response}), code
+
+
     # HANDLE ERROR ---------------------------------------------------------
     @app.errorhandler(404)
     def not_found(error):
